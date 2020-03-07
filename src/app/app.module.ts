cbbdf266871d248e758006cdfd1767323c264d12
Denis Parágas <epiccatone@gmail.com>
2020-03-07 18:23:30 +0300

    feat: add material design components support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { NgModule } from '@angular/core';
@@ -11,9 +12,10 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
     CoreModule,
-    ScullyLibModule
+    ScullyLibModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
