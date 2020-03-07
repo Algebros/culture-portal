ba8c1975630ca507a0a7c64d2de63fa3f84400f6
Denis Parágas <epiccatone@gmail.com>
2020-03-07 15:25:24 +0300

    fix: restore map component with mock and service

--- src/app/main/components/map/map.component.ts
+++ src/app/main/components/map/map.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, AfterContentInit, OnInit } from '@angular/core';
+import { MapService } from '../../services/map.service';
+import Location from './../../models/location.model';
+
+declare let ymaps: any; // tslint:disable-line:no-any
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterContentInit, OnDestroy {
 
-  constructor() { }
+  @Input() public incomeLocations: Location[];
+
+  constructor(private _mapService: MapService) { }
 
   public ngOnInit(): void {
+    this._mapService.locations = this.incomeLocations;
+  }
+
+  public ngAfterContentInit(): void {
+    ymaps.ready(this._mapService.initMap);
+  }
+
+  public ngOnDestroy(): void {
+    this._mapService.destroyMap();
   }
 
 }
---
+++ src/app/main/mocks/location-mock.ts
@@ -0,0 +1,20 @@
+import Location from '../models/location.model';
+
+export const locations: Location[] = [
+  {
+    id: 0,
+    lat: 53.875623,
+    lng: 27.607151,
+    description: 'Home',
+  }, {
+    id: 1,
+    lat: 53.927496,
+    lng: 27.683418,
+    description: 'Work',
+  }, {
+    id: 2,
+    lat: 53.876483,
+    lng: 27.630283,
+    description: 'Gym',
+  },
+];
