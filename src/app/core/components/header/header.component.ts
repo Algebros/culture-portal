af054646cee6136708361e547a02495207a065ed
Denis Parágas <epiccatone@gmail.com>
2020-03-08 22:34:40 +0300

    feat: add change language example + routing test (see header template)

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { HEADER_LABELS } from './../../constants/header.labels';
 import { Component, OnInit } from '@angular/core';
 import { HeaderLocale } from '../../models/header.model';
+import { LanguageService } from '../../../main/services/language.service';
 
 @Component({
   selector: 'app-header',
@@ -13,11 +14,15 @@ export class HeaderComponent implements OnInit {
   public labels: HeaderLocale;
   public languages: Array<string>;
 
-  constructor(public router: Router) { }
+  constructor(public router: Router, public languageService: LanguageService) { }
 
   public ngOnInit(): void {
     this.labels = HEADER_LABELS[localStorage.getItem('lang') ? localStorage.getItem('lang') : 'en'];
     this.languages = HEADER_LABELS.languages;
   }
 
+  // Change language click handler example
+  public clickHandler(event: MouseEvent): void {
+    this.languageService.changeLanguage.next(event.target.dataset.language);
+  }
 }
