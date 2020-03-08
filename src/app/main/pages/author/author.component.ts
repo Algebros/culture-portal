e03a3820cfbaef525cec9632ebf1588ba40bf8c0
EugenePochenkov <epochenkov@gmail.com>
2020-03-08 22:33:19 +0300

    feat: implement youtube video modal functionality

--- src/app/main/pages/author/author.component.ts
+++ src/app/main/pages/author/author.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {YoutubeVideoModalComponent} from '../../components/youtube-video-modal/youtube-video-modal.component';
+import Author from '../../models/author.model';
 
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss']
 })
-export class AuthorComponent implements OnInit {
+export class AuthorComponent {
+  public author: Author;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
-  public ngOnInit(): void {
+  public openModal(): void {
+   this.dialog.open(YoutubeVideoModalComponent, {
+     width: '300px',
+     height: '300px',
+     data: {
+       videoLink: this.author.youtube
+     }
+    });
   }
 
 }
