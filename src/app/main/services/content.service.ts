f474f21bcee07a4e2ccbe069f752c6659622b747
Denis Parágas <epiccatone@gmail.com>
2020-03-09 18:47:13 +0300

    feat: update getDevelopers method, inject language service, add developers variable

--- src/app/main/services/content.service.ts
+++ src/app/main/services/content.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { LanguageService } from './language.service';
 import butterService from 'src/app/core/services/butter-cms.service';
 import QueryParams from '../models/query-params.model';
+import Developer from '../models/developer.model';
 import Author from '../models/author.model';
 
 @Injectable({
@@ -8,16 +10,21 @@ import Author from '../models/author.model';
 })
 export class ContentService {
 
+  public developers: Developer[];
+
+  constructor(private languageService: LanguageService) { }
+
   public async getAuthors(params: QueryParams = { locale: 'en' }): Promise<{}>  {
     let response: {} = butterService.content.retrieve(['author'], params);
     let data: {} = await response;
     return data;
   }
 
-  public async getDevelopers(): Promise<{}> {
-    let response: {} = butterService.content.retrieve(['developer']);
-    let data: {} = await response;
-    return data;
+  public getDevelopers(): void {
+    butterService.content
+      .retrieve(['developer'], { locale: this.languageService.language })
+      .then(response => this.developers = response.data.data.developer)
+      .catch(error => console.error(error));
   }
 
   public filterAuthorsById(data: { data: { data: { author: Author[]}}}): Author[] {
