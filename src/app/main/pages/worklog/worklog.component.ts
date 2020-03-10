1b9b72265a4b8dc66a452816f0b362c6ab29e0ae
DrD1esel <kolonicki.e@gmail.com>
2020-03-10 23:51:54 +0300

    feat: add google sheets api

--- src/app/main/pages/worklog/worklog.component.ts
+++ src/app/main/pages/worklog/worklog.component.ts
@@ -1,3 +1,4 @@
+import { Observable, Subscription } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 import { IWorklog } from '../../models/worklog.model';
 import { WorklogService } from '../../services/worklog.service';
@@ -10,12 +11,13 @@ import { WorklogService } from '../../services/worklog.service';
 export class WorklogComponent implements OnInit {
 
   public worklog: IWorklog[];
+  public worklog$: Subscription;
   public columns: string[] = ['feature', 'time'];
 
   constructor(private worklogService: WorklogService) { }
 
   public ngOnInit(): void {
-    this.worklog = this.worklogService.getWorklog();
+    this.worklog$ = this.worklogService.getWorklog$().subscribe((worklog) => this.worklog = worklog);
   }
 
 }
--- src/app/main/services/worklog.service.ts
+++ src/app/main/services/worklog.service.ts
@@ -1,15 +1,67 @@
+import { HttpClient } from '@angular/common/http';
 import { IWorklog } from './../models/worklog.model';
 import { worklog } from './../../../assets/worklog/worklog';
 import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, of, from } from 'rxjs';
+import { mergeMap, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorklogService {
 
-  constructor() { }
+  private key: string = 'AIzaSyC2WjSIlrLshnULuV0pdt4xX1gx1M3fkUU';
+  private sheet: string = '1vC1GY7Ig7zZagIGY7Oxj_mpWJCg-I8c2BK21A_kOOI4';
+  private url: string = `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet}`;
+  private developers: string[];
+  private worklog: IWorklog[] = [];
+  private worklog$: BehaviorSubject<IWorklog[]> = new BehaviorSubject<IWorklog[]>([]);
+
+  constructor(private httpClient: HttpClient) {
+    this.getWorklog2();
+  }
 
   public getWorklog(): IWorklog[] {
     return worklog;
+  }
+  public getWorklog$(): Observable<IWorklog[]> {
+    return this.worklog$;
+  }
+
+  public getJobs(): void {
+    // tslint:disable-next-line: no-any
+    const getData: any = (param) => {
+      return this.httpClient.get(`${this.url}/values/'${param}'!A2:B7?key=${this.key}`, {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      });
+    };
+
+    from(this.developers).pipe(
+      mergeMap(param => getData(param))
+    // tslint:disable-next-line: no-any
+    ).subscribe((val: any) => {
+      const log: IWorklog = {name: '', works: []};
+      log.name = val.range.split('!')[0].replace(/\'/g, '');
+      val.values.forEach((job) => log.works.push({feature: job[0], time: job[1]}));
+      console.log(log);
+      console.log(this.worklog);
+      this.worklog.push(log);
+      this.worklog$.next(this.worklog);
+    });
+  }
+
+  public getWorklog2(): void {
+    this.httpClient.get(`${this.url}?key=${this.key}`, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // tslint:disable-next-line: no-any
+    }).subscribe((res: any) => {
+      this.developers = res.sheets.map((sheet) => sheet.properties.title);
+      console.log(this.developers);
+      this.getJobs();
+    });
   }
 }
