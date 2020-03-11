1ca88eaf854cba4f5409bab927d6220c6085298f
Sofroniy <Sofroniy.m@gmail.com>
2020-03-12 02:23:19 +0300

    feat: implement authors data in component

--- src/app/main/main-routing.module.ts
+++ src/app/main/main-routing.module.ts
@@ -10,9 +10,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'en' },
   { path: ':language', component: HomeComponent },
   { path: ':language/authors', component: AuthorsComponent },
-  // CHANGED
-  // { path: ':language/author/:id', component: AuthorComponent },
-  { path: ':language/author', component: AuthorComponent },
+  { path: ':language/author/:id', component: AuthorComponent },
   { path: ':language/team', component: TeamComponent },
   { path: ':language/worklog', component: WorklogComponent }
 ];
--- src/app/main/pages/author/author.component.ts
+++ src/app/main/pages/author/author.component.ts
@@ -3,29 +3,37 @@ import {VideoModalService} from '../../services/video-modal.service';
 import {ActivatedRoute, Params} from '@angular/router';
 import {ContentService} from '../../services/content.service';
 import Author from '../../models/author.model';
+import { LanguageService } from '../../services/language.service';
+import { Subscription } from 'rxjs';
 
-interface PeriodicElement {
-  title: string;
-  year: number;
-}
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss'],
 })
-export class AuthorComponent {
-
-  public ELEMENT_DATA: PeriodicElement[] = [
-    {year: 2000, title: 'Hydrogen' },
-    {year: 2000, title: 'Helium' },
-    {year: 2000, title: 'Lithium' },
-    {year: 2000, title: 'Beryllium' },
-  ];
+export class AuthorComponent implements OnInit {
+  public subscription: Subscription;
 
   public author: Author;
 
-  public displayedColumns: string[] = ['year', 'title'];
-  public dataSource: PeriodicElement[] = this.ELEMENT_DATA;
+  public displayedColumns: string[] = ['date', 'description'];
 
-  constructor(public videoModalService: VideoModalService) {}
+  constructor(
+    public videoModalService: VideoModalService,
+    private route: ActivatedRoute,
+    public contentService: ContentService,
+    private languageService: LanguageService,
+    ) { }
+
+  public ngOnInit(): void {
+    this.subscription = this.languageService.changeLanguage.subscribe(() => {
+      this.contentService.getAuthors();
+    });
+
+    this.route.params.subscribe((data) => this.author = this.contentService.getAuthorById(data.id));
+  }
+
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
