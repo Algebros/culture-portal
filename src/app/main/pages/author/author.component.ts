92085c7ea0563668d9b95461967c3f6f06261b6b
Sofroniy <Sofroniy.m@gmail.com>
2020-03-11 18:00:52 +0300

    feat: implement html/css - works, places

--- src/app/main/pages/author/author.component.ts
+++ src/app/main/pages/author/author.component.ts
@@ -2,13 +2,28 @@ import {Component} from '@angular/core';
 import Author from '../../models/author.model';
 import {VideoModalService} from '../../services/video-modal.service';
 
+interface PeriodicElement {
+  title: string;
+  year: number;
+}
 @Component({
   selector: 'app-author',
   templateUrl: './author.component.html',
   styleUrls: ['./author.component.scss']
 })
 export class AuthorComponent {
+
+  public ELEMENT_DATA: PeriodicElement[] = [
+    {year: 2000, title: 'Hydrogen' },
+    {year: 2000, title: 'Helium' },
+    {year: 2000, title: 'Lithium' },
+    {year: 2000, title: 'Beryllium' },
+  ];
+
   public author: Author;
+
+  public displayedColumns: string[] = ['year', 'title'];
+  public dataSource: PeriodicElement[] = this.ELEMENT_DATA;
 
   constructor(public videoModalService: VideoModalService) {}
 }
