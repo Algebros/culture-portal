32909ddd02cf01c2efd13d2245052308a149a32b
Sofroniy <Sofroniy.m@gmail.com>
2020-03-12 08:37:01 +0300

    feat: implement basic structure storybook

--- src/stories/components.stories.ts
+++ src/stories/components.stories.ts
@@ -1,45 +1,33 @@
-import { HeaderComponent } from 'src/app/core/components/header/header.component';
-import { CoreRoutingModule } from 'src/app/core/core-routing.module';
-import { FooterComponent } from 'src/app/core/components/footer/footer.component';
-import { moduleMetadata } from '@storybook/angular';
+import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { LanguageService } from 'src/app/main/services/language.service';
-import { DeveloperCardComponent } from 'src/app/main/components/developer-card/developer-card.component';
-import { GalleryComponent } from 'src/app/main/components/gallery/gallery.component';
-import { MapComponent } from 'src/app/main/components/map/map.component';
-import { MapService } from 'src/app/main/services/map.service';
+import { moduleMetadata } from '@storybook/angular';
+import { CoreRoutingModule } from 'src/app/core/core-routing.module';
 
 export default {
   title: 'Components',
   component: [
     HeaderComponent,
-    FooterComponent,
-    DeveloperCardComponent,
-    GalleryComponent,
-    MapComponent
   ],
   decorators: [
-    moduleMetadata({
-        imports: [
-          CommonModule,
-          CoreRoutingModule,
-          MatButtonModule,
-          MatMenuModule,
-          MatDividerModule,
-          MatIconModule,
-          MatButtonToggleModule
-        ],
-        providers: [
-          LanguageService,
-          MapService
-        ]
-    })
-  ]
+        moduleMetadata({
+            imports: [
+              CommonModule,
+              CoreRoutingModule,
+              MatButtonModule,
+              MatMenuModule,
+              MatDividerModule,
+              MatIconModule,
+              MatButtonToggleModule
+            ],
+            providers: [
+            ]
+        })
+      ]
 };
 
 export const Header = () => ({
@@ -48,41 +36,94 @@ export const Header = () => ({
 });
 
 Header.story = {
-  name: 'HeaderComponent',
+  name: 'Header',
 };
 
-export const Footer = () => ({
-  component: FooterComponent,
-  props: {},
-});
+// import { HeaderComponent } from 'src/app/core/components/header/header.component';
+// import { CoreRoutingModule } from 'src/app/core/core-routing.module';
+// import { FooterComponent } from 'src/app/core/components/footer/footer.component';
+// import { moduleMetadata } from '@storybook/angular';
+// import { CommonModule } from '@angular/common';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatMenuModule } from '@angular/material/menu';
+// import { MatDividerModule } from '@angular/material/divider';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatButtonToggleModule } from '@angular/material/button-toggle';
+// import { LanguageService } from 'src/app/main/services/language.service';
+// import { DeveloperCardComponent } from 'src/app/main/components/developer-card/developer-card.component';
+// import { GalleryComponent } from 'src/app/main/components/gallery/gallery.component';
+// import { MapComponent } from 'src/app/main/components/map/map.component';
+// import { MapService } from 'src/app/main/services/map.service';
 
-Footer.story = {
-  name: 'FooterComponent',
-};
+// export default {
+//   title: 'Components',
+//   component: [
+//     HeaderComponent,
+//     FooterComponent,
+//     DeveloperCardComponent,
+//     GalleryComponent,
+//     MapComponent
+//   ],
+//   decorators: [
+//     moduleMetadata({
+//         imports: [
+//           CommonModule,
+//           CoreRoutingModule,
+//           MatButtonModule,
+//           MatMenuModule,
+//           MatDividerModule,
+//           MatIconModule,
+//           MatButtonToggleModule
+//         ],
+//         providers: [
+//           LanguageService,
+//           MapService
+//         ]
+//     })
+//   ]
+// };
 
-export const DevCard = () => ({
-  component: DeveloperCardComponent,
-  props: {},
-});
+// export const Header = () => ({
+//   component: HeaderComponent,
+//   props: {},
+// });
 
-DevCard.story = {
-  name: 'DevCardComponent',
-};
+// Header.story = {
+//   name: 'HeaderComponent',
+// };
 
-export const Gallery = () => ({
-  component: GalleryComponent,
-  props: {},
-});
+// export const Footer = () => ({
+//   component: FooterComponent,
+//   props: {},
+// });
 
-Gallery.story = {
-  name: 'GalleryComponent',
-};
+// Footer.story = {
+//   name: 'FooterComponent',
+// };
 
-export const Map = () => ({
-  component: MapComponent,
-  props: {},
-});
+// export const DevCard = () => ({
+//   component: DeveloperCardComponent,
+//   props: {},
+// });
 
-Map.story = {
-  name: 'MapComponent',
-};
+// DevCard.story = {
+//   name: 'DevCardComponent',
+// };
+
+// export const Gallery = () => ({
+//   component: GalleryComponent,
+//   props: {},
+// });
+
+// Gallery.story = {
+//   name: 'GalleryComponent',
+// };
+
+// export const Map = () => ({
+//   component: MapComponent,
+//   props: {},
+// });
+
+// Map.story = {
+//   name: 'MapComponent',
+// };
