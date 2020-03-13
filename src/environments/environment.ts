a5156fc7f61dfa8ae7156ad80ebdcb505d4be0d2
Anhelina Lakhtanava <linobanana@gmail.com>
2020-03-13 16:37:23 +0300

    fix: replace all tokens & urls to env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,10 @@
 
 export const environment = {
   production: false,
-  BUTTER_TOKEN: 'ccab3eb57bd8bed305f2371c2b2250d0e02d7737'
+  BUTTER_TOKEN: 'ccab3eb57bd8bed305f2371c2b2250d0e02d7737',
+  SHEET: '1vC1GY7Ig7zZagIGY7Oxj_mpWJCg-I8c2BK21A_kOOI4',
+  GOOGLESHEETS_URL: 'https://sheets.googleapis.com/v4/spreadsheets/',
+  GOOGLESHEETS_API_KEY: 'AIzaSyC2WjSIlrLshnULuV0pdt4xX1gx1M3fkUU'
 };
 
 /*
